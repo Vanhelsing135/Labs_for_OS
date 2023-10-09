5a98290097be1355d21b11ba10d9fb3750d1da91
Ivan Filon <vanbest27@gmail.com>
2023-10-09 19:42:51 +0300

    editing main.cpp

--- Lab1_win/main.cpp
+++ Lab1_win/main.cpp
@@ -13,43 +13,8 @@ int main(VOID) {
     }
 
     WaitForSingleObject(processInfo.hProcess, INFINITE);
-    printf("Subprocess terminated with code:");
-    fflush(stdout);
-    GetExitCodeProcess(processInfo.hProcess, &exitCode);
     CloseHandle(processInfo.hProcess);
     CloseHandle(processInfo.hThread);
     return 0;
 }
 
-/*#include <stdio.h>
-#include <Windows.h>
-
-int main(int argc, char** argv)
-{
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
-
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si);
-	ZeroMemory(&pi, sizeof(pi));
-
-	if (!CreateProcess(
-		L"C:\\Users\\vanbe\\source\\repos\\lab1_OS\\x64\\Debug\\process.exe",
-		NULL,
-		NULL,
-		FALSE,
-		0,
-		NULL,
-		NULL,
-		&si,
-		&pi))
-	{
-		printf("CreateProcess failed (%d).\n", GetLastError());
-	}
-
-	WaitForSingleObject(pi.hProcess, INFINITE);
-
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
-	return 0;
-}*/
